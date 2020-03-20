77ad4d6a4f3aacd784498604ee902f3805a5f34d
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-20 11:56:48 -0700

    added App class to app.js, defined handleGetGrades success/error methods, getGrades and start methods

---
+++ components/app.js
@@ -0,0 +1,26 @@
+class App {
+  constructor () {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {
+        "X-Access-Token": "GkNqAZE8"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+  start() {
+    this.getGrades();
+  }
+}
