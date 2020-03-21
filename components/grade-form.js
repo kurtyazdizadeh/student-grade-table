b8db404464843dc430bc07aee483d8c38125cbfc
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-20 17:17:02 -0700

    added event listener to GradeForm class for Submit, tested functional

--- components/grade-form.js
+++ components/grade-form.js
@@ -3,6 +3,8 @@ class GradeForm {
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.formElement = formElement;
+
+    this.formElement.addEventListener('submit', this.handleSubmit);
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
