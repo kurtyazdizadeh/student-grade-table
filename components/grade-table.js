920854a68d55b0abf662ad14d4f8ae7d5ae1e226
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-22 13:42:57 -0700

    edited HTML and grade-table.js to add column for Operations and Delete button

--- components/grade-table.js
+++ components/grade-table.js
@@ -11,12 +11,19 @@ class GradeTable {
       var name = document.createElement("td");
       var course = document.createElement("td");
       var grade = document.createElement("td");
+      var operations = document.createElement("td");
+
+      var operationsButton = document.createElement("button");
+      operationsButton.textContent = "Delete";
+      operationsButton.className = "btn btn-danger";
+
+      operations.appendChild(operationsButton);
 
       name.textContent = grades[i].name;
       course.textContent = grades[i].course;
       grade.textContent = grades[i].grade;
 
-      var rowElements = [name, course, grade];
+      var rowElements = [name, course, grade, operations];
 
       for (var rowIndex = 0; rowIndex < rowElements.length; rowIndex++){
         row.appendChild(rowElements[rowIndex]);
