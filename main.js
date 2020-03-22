464533a147e85e1c7bfee9fd0d3a734f9e1e3733
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-22 14:37:43 -0700

    added noGrades p tag as an argument in main.js of GradeTable instantiation, gave p tag an id of noGrades on HTML

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var tableEl = document.querySelector('table');
 var headerEl = document.querySelector('header');
 var formEl = document.querySelector('form');
+var noGradesEl = document.querySelector("#noGrades");
 
-var gradeTable = new GradeTable(tableEl);
+var gradeTable = new GradeTable(tableEl, noGradesEl);
 var pageHeader = new PageHeader(headerEl);
 var gradeForm = new GradeForm(formEl);
 var app = new App(gradeTable, pageHeader, gradeForm);
