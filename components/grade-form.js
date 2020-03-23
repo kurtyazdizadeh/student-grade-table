04d7969b4f056792fbd7024d0b8936402bb4642a
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-23 12:15:35 -0700

    successfully able to pull data from clicked row back onto the form. Still need to add proper ajax call

--- components/grade-form.js
+++ components/grade-form.js
@@ -5,7 +5,11 @@ class GradeForm {
 
     this.formElement = formElement;
 
-    this.formElement.addEventListener('submit', this.handleSubmit);
+    this.submitBtn = this.formElement.querySelector("#formSubmit");
+    this.updateBtn = this.formElement.querySelector("#formUpdate");
+
+    this.submitBtn.addEventListener('submit', this.handleSubmit);
+    this.updateBtn.addEventListener('submit', this.handleUpdate);
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
@@ -29,17 +33,36 @@ class GradeForm {
     var name = this.formElement.querySelector("#name");
     var course = this.formElement.querySelector("#course");
     var grade = this.formElement.querySelector("#grade");
-    var submitBtn = this.formElement.querySelector("#formSubmit");
+    var updateBtn = this.formElement.querySelector("#formUpdate");
     var formTitle = this.formElement.querySelector("#formTitle");
 
     name.setAttribute("value", data.name);
     course.setAttribute("value", data.course);
     grade.setAttribute("value", data.grade);
-    submitBtn.textContent = "Update";
+
     formTitle.textContent = "Update Grade";
+    this.submitBtn.classList.add("d-none");
+    updateBtn.classList.remove("d-none");
+  }
 
-    console.log(name);
-    console.log(data);
+  handleUpdate(event){
+    event.preventDefault();
 
+    var formData = new FormData(event.target);
+    var name = formData.get("name");
+    var course = formData.get("course");
+    var grade = formData.get("grade");
+
+    grade = parseInt(grade);
+
+    this.updateGrade({
+      "name": name,
+      "course": course,
+      "grade": grade
+    });
+    event.target.reset();
+
+    this.updateBtn.classList.add("d-none");
+    this.submitBtn.classList.remove("d-none");
   }
 }
